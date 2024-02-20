23913eb40097d7536984646d68e97240eb81597a
Ilya <WestDragon@github.com>
2024-02-20 21:55:41 +0300

    Объединение изменений из удаленного репозитория

--- src/home_work_12/Main1.java
+++ src/home_work_12/Main1.java
@@ -8,7 +8,7 @@ public class Main1 {
         System.out.println("Введите число а");
         int a = console.nextInt(); //40 - 0010 1000
         System.out.println("Введите число b");
-        int b = console.nextInt(); //82 - 0101 0010 - это пример
+        int b = console.nextInt(); //82 - 0101 001git statusпример
         int y = a & b; // 0 - 0000 000
         int n = a | b; // 122 - 0111 1010
 
